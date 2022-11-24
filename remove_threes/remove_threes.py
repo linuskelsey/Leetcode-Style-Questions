de12663d58d2b7b8b5f31eb83cdd57d0989b7bc7
Linus Kelsey <alfred.linus.kelsey@gmail.com>
2022-11-24 11:00:29 +0000

    Fix IndexError when split the final substring

--- remove_threes/remove_threes.py
+++ remove_threes/remove_threes.py
@@ -2,14 +2,16 @@ def split_string(s):
 	substrings=[]
 	L=len(s)
 	for i in range(L):
-	 	k=0
-	 	substr=''
-	 	while s[i] == s[i+k]:
-	 		substr += s[i+k]
-	 		k+=1
-	 	i += k
-	 	substrings.append(substr)
-	 	print(substrings)
+		k=0
+		substr=''
+		try:
+			while s[i] == s[i+k]:
+				substr += s[i+k]
+				k+=1
+			i += k
+			substrings.append(substr)
+		except IndexError:
+			substrings.append(s[i:])
 	return substrings
 
-print(split_string('aaabbaabbbc'))
+print(split_string('aaabbaabbbcc'))
