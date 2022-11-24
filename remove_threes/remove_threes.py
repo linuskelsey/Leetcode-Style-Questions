95f2f5af8399a2b6bf04e1ec1e5da856a8e863af
Linus Kelsey <alfred.linus.kelsey@gmail.com>
2022-11-24 12:20:36 +0000

    Create much cleaner code to remove threes

--- remove_threes/remove_threes.py
+++ remove_threes/remove_threes.py
@@ -1,23 +1,17 @@
-def split_string(s):
-	substrings=[]
-	L=len(s)
-	for i in range(L):
-		if s:
-			k=0
-			substr=''
-			try:
-				while s[0] == s[k]:
-					substr+=s[k]
-					k+=1
-			except IndexError:
-				substrings.append(s)
-			s=s[k:]
-			substrings.append(substr)
-	return substrings[:-1]
-
 def remove_threes(s):
-	strings = split_string(s)
-	for string in strings:
-		if len(string) >= 3:
-			strings.remove(string)
-	return ''.join(strings)
+
+	def remove_first_three(s):
+		for i in range(len(s)-2):
+			if s[i] == s[i+1] and s[i] == s[i+2]:
+				s = s[:i] + s[i+3:]
+				break
+		return s
+
+	threes = True
+	while threes:
+		if s == remove_first_three(s):
+			threes = False
+		else:
+			s = remove_first_three(s)
+
+	return s
